0af9f31c71fa16401c0693893cc59f172628bcd6
John Larson <jlarson@veloracer.com>
2017-10-16 12:49:10 -0500

    Formatting changes.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 const app = express();
-const routes = require('./api/routes/DataRoute'); //importing route
+const routes = require( './api/routes/DataRoute' ); // importing route
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use( bodyParser.urlencoded( { 'extended': true } ) );
+app.use( bodyParser.json() );
 
-routes(app); //register the route
-app.listen(port);
+routes( app ); // register the route
+app.listen( port );
 
-console.log('RESTful API server started on: ' + port);
+console.log( `RESTful API server started on: ${ port}` );
